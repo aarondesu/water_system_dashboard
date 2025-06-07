90e101f1416764f19c7c6b0de59a93be7d29117c
aarondesu <aaronneielaro@gmail.com>
2025-06-07 11:38:10 +0800

    refactor: add derive apis from baseApis to share tag types

--- app/redux/store.ts
+++ app/redux/store.ts
@@ -1,29 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { authApi } from "./apis/authApi";
+import { baseApi } from "./apis/baseApi";
 import { utilityApi } from "./apis/utilityApi";
-import { userApi } from "./apis/userApi";
-import { subscriberApi } from "./apis/subscriberApi";
-import { meterApi } from "./apis/meterApi";
-import { readingApi } from "./apis/readingApi";
 
 export const store = configureStore({
   reducer: {
-    [authApi.reducerPath]: authApi.reducer,
+    [baseApi.reducerPath]: baseApi.reducer,
     [utilityApi.reducerPath]: utilityApi.reducer,
-    [userApi.reducerPath]: userApi.reducer,
-    [subscriberApi.reducerPath]: subscriberApi.reducer,
-    [meterApi.reducerPath]: meterApi.reducer,
-    [readingApi.reducerPath]: readingApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat([
-      authApi.middleware,
-      utilityApi.middleware,
-      userApi.middleware,
-      subscriberApi.middleware,
-      meterApi.middleware,
-      readingApi.middleware,
-    ]),
+    getDefaultMiddleware().concat([baseApi.middleware, utilityApi.middleware]),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
